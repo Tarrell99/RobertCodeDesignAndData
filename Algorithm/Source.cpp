ef7415b296e7d68ac138e11a670e7e82473679fe
tarrellriley <tarrellriley@gmail.com>
2018-10-22 17:59:28 -0500

    work

--- Algorithm/Source.cpp
+++ Algorithm/Source.cpp
@@ -10,7 +10,7 @@ int Player::players(int hp)
 {
 	for (int i = 0; i < 10; i++)
 	{
-		for (int j = i + 1; j < 10;)
+		for (int j = i + 1; j < 10; j++)
 		{
 			int temp;
 			if (i < j)
@@ -18,16 +18,21 @@ int Player::players(int hp)
 				temp = i;
 				i = j;
 				j = temp;
-				i++;
+				
 			}
 		}
 	}
-	return 0;
+	return hp ;
 }
 
 int main()
 {
-	
+	int Hparry[10];
+	// call the function on an arry where the arry has the Hps of the playes and it sourts them in 
+	// desending order and return the player with the lest Hp.
+	Player* player1 = new Player;
+	Player* player2 = new Player;
+	player1->players(10);
 
 
 }
